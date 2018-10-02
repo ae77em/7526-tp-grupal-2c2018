80360261248f7e88d594f5808683159d2e1d5c1a
Federico Ferreyra <federico@grayshirts.com.ar>
2018-10-02 18:05:23 -0300

    avance 9 pero todo mal

--- tp1/respuesta9_paso1.py
+++ tp1/respuesta9_paso1.py
@@ -1,7 +1,9 @@
 import plotly.plotly as py
 import plotly.graph_objs as go
+import random
 
-import numpy as np
+from math import log
+from math import exp
 
 
 def generador_congruencial_lineal(x_n):
@@ -12,6 +14,14 @@ def generador_congruencial_lineal(x_n):
     x = float(((a * x_n) + c) % m) / float(m)
 
     return x
+
+def calcularEst(Oi,Ei):
+
+    return ( pow( Oi - Ei ,2) ) / float(Ei)
+
+def obtenerProbabilidadDeGap(nroDeGap):
+
+    return (0.8)*pow(0.2,nroDeGap)
 
 
 x_n = (90697 + 89563) // 2
@@ -24,13 +34,14 @@ gapsSegundoIntervalo = []  # array de nro generado
 contadorSegundoIntervalo = 0
 aparecioPrimerNroEnSegundoRango = False
 
-for i in range(1000):
+for i in range(30):
     x_n = generador_congruencial_lineal(x_n)
 
     if x_n >= 0.2 and x_n <= 0.6:
         if aparecioPrimerNroEnPrimerRango == False:
             aparecioPrimerNroEnPrimerRango = True
         else:
+            contadorPrimerIntervalo -= 1
             gapsPrimerIntervalo.append(contadorPrimerIntervalo)
             contadorPrimerIntervalo = 0
 
@@ -38,15 +49,19 @@ for i in range(1000):
         if aparecioPrimerNroEnSegundoRango == False:
             aparecioPrimerNroEnSegundoRango = True
         else:
+            contadorSegundoIntervalo -= 1
             gapsSegundoIntervalo.append(contadorSegundoIntervalo)
             contadorSegundoIntervalo = 0
 
+    #si aparecio por primera vez, va incrementandose hasta q se resetea
     if aparecioPrimerNroEnPrimerRango == True:
         contadorPrimerIntervalo += 1
 
     if aparecioPrimerNroEnSegundoRango == True:
         contadorSegundoIntervalo += 1
 
+maxGap = 0
+totalGap = 0
 counters = {}
 
 for unGap in gapsPrimerIntervalo:
@@ -55,11 +70,45 @@ for unGap in gapsPrimerIntervalo:
     else:
         counters[str(unGap)] = 1
 
+    if maxGap < unGap:
+        maxGap = unGap
+
 for unGap in gapsSegundoIntervalo:
     if str(unGap) in counters:
         counters[str(unGap)] = counters[str(unGap)] + 1
     else:
         counters[str(unGap)] = 1
 
-print counters
+    if maxGap < unGap:
+        maxGap = unGap
+
+
+#creo array del contenido como tantos gaps existan
+contadorGaps = [0] * (maxGap + 1)
+
+#sort gaps de menor a mayor
+for key, value in counters.iteritems():
+    contadorGaps[int(key)] = value
+    totalGap += value
+
+print "TOTAL GAPS"
+print totalGap
+print ""
+print "GAPS"
+print contadorGaps
+
+print("i" ,"p","Oi","Ei")
+sumar = 0
+for key, value in enumerate(contadorGaps):
+    probabilidadDeGap = obtenerProbabilidadDeGap(key)
+    Ei = probabilidadDeGap * totalGap
+    Oi = value
+
+    sumar += Ei
+
+    print(key ,probabilidadDeGap,Oi,Ei, calcularEst(Oi,Ei))
+
+print ""
+print "SUM Ei"
+print sumar
 
