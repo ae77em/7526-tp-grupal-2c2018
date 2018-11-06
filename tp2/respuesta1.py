5178a55b3d59159972c523f064a8a73e2374a68b
Esteban <aeeisner@yahoo.com.ar>
2018-11-06 00:11:03 -0300

    Cierro el punto 1.

--- tp2/respuesta1.py
+++ tp2/respuesta1.py
@@ -1,15 +1,12 @@
+"""
+Punto 1
+
+"""
 import numpy
 import random
 import matplotlib.pylab as plt
 
-"""
- El tiempo medio de espera entre que la solicitud llega y puede ser procesada (suponer que ninguna conexion se
-cae por timeout). -> Media de espera en la cola
-
- La fraccion de las solicitudes que no esperaron para ser procesadas. P(N(t)=0)
-"""
-
-arrives = 100
+arrives = 100000
 
 def computeQueue(a, C, Q0=0): #  initial queue length is 0
     N = len(a)
@@ -32,35 +29,27 @@ def waitingTime(At,Dt):
     m = min(len(Ak), len(Dk))
     W = Dk[:m] - Ak[:m]
     
-    return Ak, Dk, W
+    return W
 
 def capacidad_servidor(_lambda=1, _mu=1, _arrives=100000):
     baseArrivals = numpy.random.poisson(_lambda, size=int(_arrives))
     baseCapacity = _mu*numpy.ones_like(baseArrivals)
 
     queueSizes, departures = computeQueue(baseArrivals, baseCapacity)
-    totalArrivals = numpy.cumsum(baseArrivals) # compute the total number of arrivals up to time n
-    totalDepartures = totalArrivals - queueSizes
-
-    aux = float(queueSizes.tolist().count(0)) / float(_arrives)
-    print("%% Solicitudes que no esperaron -> %f" % aux)
-
-    #plt.plot(totalArrivals, linestyle='--', drawstyle = 'steps-pre', label = 'Arrivals')
-    #plt.plot(totalDepartures, linestyle='-.', drawstyle = 'steps-mid', label = 'Departures')
-    #plt.plot(queue, linestyle=':', drawstyle = 'steps-post', label = 'Queue')
-    #plt.legend()
-    #plt.show()
-
-    Ak, Dk, W = waitingTime(baseArrivals, departures)
+        
+    baseWaitingTime = waitingTime(baseArrivals, departures)
     
-    print("Tiempo medio de espera -> %f" % W.mean())
-    #plt.plot(Ak, label='Ak')
-    #plt.plot(Dk, label='Dk')
-    #plt.plot(W, label='w')
-    #plt.legend()
-    #plt.plot()
-    #plt.show()
+    print("El tiempo medio de espera es de %f segs" % baseWaitingTime.mean())
+    aux = float(queueSizes.tolist().count(0)) / float(_arrives)
+    print("Las solicitudes que no esperaron son el %f%%" % aux)
 
+    resolutionTime = numpy.array([x + y for x, y in zip(baseWaitingTime, baseCapacity)])
+    print("El tiempo de resolucion promedio es %f segs" % resolutionTime.mean())
+    print("")
+
+#
+# Hago las pruebas de cada caso
+#   
 p = 0.6
 q = 1 - p
     
@@ -75,3 +64,8 @@ capacidad_servidor(_lambda=lambdaA2,_mu=muA2, _arrives=int(arrives*q))
 lambdaB = 1.0/4.0
 muB = 1.0/0.8
 capacidad_servidor(_lambda=lambdaB,_mu=muB, _arrives=int(arrives))
+
+"""
+FIN punto 1
+
+"""
